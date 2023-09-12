6af256f7c9ab0a937ca9539bacb4e1ebc5c93ea7
Yijia Chen <kevinc110604@gmail.com>
2023-09-12 11:09:18 -0700

    [new] Change to view 1 queue at a time

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { Text, HStack, Box, Flex } from "@chakra-ui/react";
+import { Text, HStack, Flex, IconButton, VStack, Divider, Button  } from "@chakra-ui/react";
+import { TriangleUpIcon, TriangleDownIcon } from '@chakra-ui/icons';
 import { Layout } from "./components/Layout";
 import { fetchWrapper } from "./utils/fetchWrapper";
 
 export const App = () => {
   const [queues, setQueues] = useState<string[]>([]);
+  const [currentQueue, setCurrentQueue] = useState("");
   const [jobs, setJobs] = useState<Record<string, any[]>>({});
 
   useEffect(() => {
@@ -40,27 +42,54 @@ export const App = () => {
       ) : typeof jobs[queues[0]] === 'undefined' || queues.length !== Object.keys(jobs).length ? (
         <Text>{"Loading..."}</Text>
       ) : (
-        <HStack spacing='24px'>
-          {queues.map((q) => (
-            <Box key={q}>
-              <Text key={q}>{q}</Text>
-              {jobs[q].map((j, index) => (
-                <Flex
-                  key={index} 
-                  w="500px"
-                  p={5} 
-                  shadow="md" 
-                  borderWidth="1px" 
-                  borderRadius="7px"
-                >
-                  <Box>
-                    <Text key={j}>{JSON.stringify(j).substring(0, 50)}</Text>
-                  </Box>
-                </Flex>
-              ))}
-            </Box>
-          ))}
-        </HStack>
+        <Flex h={1000}> 
+          <VStack spacing='24px' p={5}>
+            {queues.map((q, index) => (
+              <Button 
+                key={index} 
+                w={120} 
+                onClick={() => { setCurrentQueue(q); }} 
+                isActive={currentQueue===q} 
+                _active={{bg:'teal',transform: 'scale(0.98)', borderColor: 'teal',}}
+              >
+                {q}
+              </Button>
+            ))}
+          </VStack>
+          <Divider orientation="vertical"/>
+          <VStack spacing='24px' p={5} >
+            {currentQueue === "" ? (
+              <Text>{"Select a queue to view its items"}</Text>
+            ) : jobs[currentQueue].map((j, index) => {
+              let jsonText = JSON.stringify(j);
+              return (
+                <HStack>
+                  <VStack >
+                    <IconButton
+                      aria-label='Move upward'
+                      icon={<TriangleUpIcon/>}
+                      size='s'
+                      p={1}
+                      fontSize="15px"
+                      onClick={() => { }}
+                    />
+                    <IconButton
+                      aria-label='Move downward'
+                      icon={<TriangleDownIcon/>}
+                      size='s'
+                      p={1}
+                      fontSize="15px"
+                      onClick={() => { }}
+                    />
+                  </VStack>
+                  <Button key={index} w={500} h={61}>
+                    {jsonText.length < 50 ? jsonText : jsonText.substring(0, 50)+" ..."}
+                  </Button>
+                </HStack>
+              );
+            })}
+          </VStack>
+        </Flex>
       )}
     </Layout>
   );
